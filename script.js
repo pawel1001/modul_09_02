3373933e450a4591a15c143d26bfffb6c558753b
pawel <pawel1001@vp.pl>
2016-12-07 17:00:14 +0100

    Carousel v1.0

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-var $span = $("span");
+jQuery(function($) { 
+  var $slider = $('.slider');
+  var $slide = 'li'; 
+  var $transition_time = 1000; 
+  var $time_between_slides = 4000;
+ 
+ function slides(){
+    return $slider.find($slide);
+  }
 
-$span.each(function(index, element) {
-	if (index % 2 == 0) {
-		$(element).css('color', 'red');
-	};
-});
+  slides().fadeOut();
 
-var paragraphs = $('p');
+  slides().first().addClass('active');
+  slides().first().fadeIn($transition_time);
 
-paragraphs.each (function(index, element) {
-var button = '<button class="btn" data-tmp="' + index + '"> Click me </button>';
-$(element).append(button);
-} );
+  $interval = setInterval(
+    function(){
+      var $i = $slider.find($slide + '.active').index();
 
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
+      slides().eq($i).removeClass('active');
+      slides().eq($i).fadeOut($transition_time);
+
+      if (slides().length == $i + 1) $i = -1; 
+
+      slides().eq($i + 1).fadeIn($transition_time);
+      slides().eq($i + 1).addClass('active');
+    }
+    , $transition_time +  $time_between_slides 
+  );
+
 });
